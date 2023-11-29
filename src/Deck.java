34436b3932934a6e9822dcf35af8527cdea6445b
Josh Little <josh.little@menloschool.org>
2023-11-28 22:51:46 -0800

    Created a card constructor.

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+    private static int cardsLeft;
+
+    public Deck(String[] ranks, String[] suits, int[] points) {
+       cards = new ArrayList<Card>();
+       for (int i = 0; i < suits.length; i++) {
+           for (int j = 0; j < ranks.length; j++) {
+               cards.add(new Card(ranks[j], suits[i], points[j]));
+           }
+       }
+       cardsLeft = cards.size();
+    }
+
+    public boolean isEmpty() {
+        if (cardsLeft == 0)
+            return true;
+        return false;
+
+    }
+
+    public int getCardsLeft() {
+        return cardsLeft;
+    }
+
+    public Card deal() {
+        if (isEmpty())
+            return null;
+        cardsLeft--;
+        return cards.get(cardsLeft);
+    }
+
+    public void shuffle() {
+        for (int i = cards.size() - 1; i >= 0; i--) {
+            int randomIndex = (int)(Math.random() * cards.size());
+            cards.set(i, cards.remove(randomIndex));
+            cards.set(randomIndex, cards.remove(i - 1));
+
+        }
+        cardsLeft = cards.size();
+    }
+
 }
